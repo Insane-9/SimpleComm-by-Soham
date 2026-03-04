6594240706a7c7868d082cbb2821d98f01873a39
Soham13anerjee <sohambanerg@gmail.com>
2026-03-05 00:05:28 +0530

    latest commit

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,42 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import connectDB from './config/db.js';
-import authRoutes from './routes/authRoutes.js';
-import productRoutes from './routes/productsRoutes.js';
-import cartRoutes from './routes/cart.js';
-import addressRoutes from './routes/address.js';
-import orderRoutes from './routes/order.js';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
+import productRoutes from "./routes/productsRoutes.js";
+import cartRoutes from "./routes/cart.js";
+import addressRoutes from "./routes/address.js";
+import orderRoutes from "./routes/order.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = process.env.PORT || 5001;
 
-app.use(cors());
-app.use(express.json());
-app.use('/api/auth', authRoutes);
-app.use('/api/products', productRoutes);
-app.use('/api/cart', cartRoutes);
-app.use('/api/address', addressRoutes);
-app.use('/api/order',orderRoutes )
+if (process.env.NODE_ENV !== "production") {
+  app.use(cors());
+}
 
-app.get('/', (req, res) => {
-    res.send('API is running...');
-});
+app.use(express.json());
+app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
+app.use("/api/address", addressRoutes);
+app.use("/api/order", orderRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.use((req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 connectDB();
 
 app.listen(port, () => {
-    console.log('Server is running on port 5001');
+  console.log("Server is running on port 5001");
 });
